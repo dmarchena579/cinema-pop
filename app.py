c4161546d9e27c4c1e433f8ee349b0a6ff613f5c
Newton Ni <nwtnni@gmail.com>
2018-11-07 21:17:38 -0500

    Update app.py to unpack data files before launching server

--- app.py
+++ app.py
@@ -1,5 +1,24 @@
-from app import app, socketio
+import os
+import zipfile
+import tempfile
+
+def unpack():
+    files = []
+    for i in range(1, 5):
+        files.append("app/static/data-" + str(i) + ".zip.part")
+
+    with tempfile.TemporaryFile() as temp:
+        for f in files:
+            with open(f) as infile:
+                temp.write(infile.read())
+    
+        zipped = zipfile.ZipFile(temp)
+        zipped.extractall("app/static")
+        zipped.close()
 
 if __name__ == "__main__":
-  print "Flask app running at http://0.0.0.0:5000"
-  socketio.run(app, host="0.0.0.0", port=5000)
+    if not os.path.isdir("app/static/data"):
+        unpack()
+
+    from app import app, socketio
+    socketio.run(app, host="0.0.0.0", port=5000)
